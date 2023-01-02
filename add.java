360aff43c8f5323c15f822051edf13b7c5838e22
malathi <malathireddy5432@gmail.com>
2023-01-02 10:06:50 +0000

    modified java code

--- add.java
+++ add.java
@@ -1,15 +1,15 @@
 import java.util.*;
-
 public class Main {
-    
     public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);//used to take the input
         
-        int x = 89; 
+        System.out.println("Enter the first number:");
+        int x = scn.nextInt(); //reads the input
         
-        int y = 43;
+        System.out.println("Enter the second number:");
+        int y = scn.nextInt(); //reads the input
         
-        int sum = x + y;
-        
+        int sum = x + y; //add the two numbers
         System.out.println("Sum is " + sum);
     }
 }
